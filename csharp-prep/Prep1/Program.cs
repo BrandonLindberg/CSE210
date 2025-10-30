bc6780faa23ff3252c504527f21c70501401a88a
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2025-10-30 10:46:35 -0600

    Prep1 PRogram

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,16 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
         string firstName;
         string lastName;
 
         Console.Write("Please enter your first name: ");
         firstName = Console.ReadLine();
 
-        Console.Write("Pleae enter your last name: ");
+        Console.Write("Please enter your last name: ");
         lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {firstName} {lastName}");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
     }
 }
