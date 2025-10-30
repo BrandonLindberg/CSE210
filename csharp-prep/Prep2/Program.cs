f62eb2605381d309363b7c7bb3ae2c95c1c328c6
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2025-10-30 10:59:19 -0600

    Prep2 letter grade calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string gradePercentString = Console.ReadLine();
+
+        int gradePercent = int.Parse(gradePercentString);
+        string letterGrade;
+
+        if (gradePercent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is a(n) {letterGrade}");
     }
 }
