81771a09e93ce28a2c5c9a385e05d29ad73495ee
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2025-11-04 13:53:11 -0700

    solar system and planets

--- sandbox/SolarSystem/Program.cs
+++ sandbox/SolarSystem/Program.cs
@@ -2,6 +2,19 @@
 {
     static void Main()
     {
-        Console.WriteLine("Bonjour tout le monde");
+        // Console.WriteLine("Bonjour tout le monde");
+        Planet mercury = new Planet();
+        mercury._name = "Mercury";
+        mercury._diameter = 123;
+
+        Planet venus = new Planet();
+        venus._name = "Venus";
+        venus._diameter = 45;
+
+        mercury.DisplayPlanetInformation();
+
+        SolarSystem solarSystem = new SolarSystem();
+        solarSystem._solarSystem.Add(mercury);
+        solarSystem._solarSystem.Add(venus);
     }
 }
