6755f37111d6c6ad607a73a690ab57ce559d4f1e
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2025-11-04 09:44:54 -0700

    prep and solarsystem program creation

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,44 +3,15 @@ using System;
 class Program
 {
 
-    static void TestValues(int x, int y, double z)
+    static int AddValues(int n1, int n2, int n3)
     {
-        Console.WriteLine($"The values are: {x}, {y}, {z}");
+        int total = n1 + n2 + n3;
+        return total;
     }
 
     static void Main(string[] args)
     {
-        // int x = 10;
-        // Console.WriteLine($"{x}, {++x}, {x++}, {x}");
-        // Console.WriteLine(Math.Pow(2, 64));
-
-        // for (int i = -10000; i <= 10000; i += 100)
-        // {
-        //     Console.WriteLine($"i is: {i}");
-        // }
-
-        // bool done = false;
-
-        // while (!done)
-        // {
-        //     Console.Write("Input your age: ");
-        //     int age = int.Parse(Console.ReadLine());
-        //     if (age >= 0 && age <= 125)
-        //     {
-        //         done = true;
-        //         Console.WriteLine($"Your age is {age}");
-        //     }
-        // }
-
-        // do
-        // {
-        //     Console.Write("Input your age: ");
-        //     int age = int.Parse(Console.ReadLine());
-        //     if (age >= 0 && age <= 125)
-        //     {
-        //         done = true;
-        //         Console.WriteLine("Nice age");
-        //     }
-        // } while (!done);
+        int total = AddValues(10, 3, 56);
+        Console.WriteLine(total);
     }
 }
