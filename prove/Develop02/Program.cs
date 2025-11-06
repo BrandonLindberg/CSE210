2f711dca173b51fd4a491fc4dec9a8efe49420dc
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2025-11-06 10:00:13 -0700

    test

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -6,6 +6,35 @@ class Program
     {
         Menu journalMenu = new Menu();
 
-        journalMenu.ProcessMenu();
+        int userSelection;
+
+        bool done = false;
+
+        do
+        {
+            userSelection = journalMenu.ProcessMenu();
+
+            switch (userSelection)
+            {
+                case 1:
+                //create new entry object
+                //call create on object
+                //add entry to journal
+                    break;
+                case 2:
+                //call journal.Display
+                    break;
+                case 3:
+                //save to file
+                    break;
+                case 4:
+                //read from file
+                    break;
+                case 5:
+                    done = true;
+                    break;
+            }
+
+        } while (!done);
     }
 }
